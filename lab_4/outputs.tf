16c27cc862d7694ec5becbbf08c98eb88360dd5f
Clarence <subiaclarence96@gmail.com>
2023-11-22 14:12:23 +0800

    Lab 4

---
+++ lab_4/outputs.tf
@@ -0,0 +1,15 @@
+output "name" {
+  value = "Instance Name: ${aws_instance.amazon.key_name}"
+}
+
+output "public_dns" {
+  value = aws_instance.amazon.public_dns
+}
+
+output "public_ip" {
+  value = aws_instance.amazon.public_ip
+}
+
+output "ssh_connect" {
+  value = "ssh -i ~/.ssh/id_rsa ec2-user@$(terraform output -raw public_ip)"
+}
